d3f5dbe5c06cc66f07462045dc701e78a4aeb97d
unknown <shaonkabir98@gmail.com>
2019-08-26 20:00:21 -0700

    Bootstrap and fontawesome added by using CDN

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,19 @@
-import Items from '../components/Items'
-const Home = () => (
-    <div>
-        <Items />
-    </div>
-)
+import Head from "next/head";
+import Items from "../components/Items";
+
+const Home = props => (
+  <div>
+    <Head>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      />
+    </Head>
+    <Items page={parseFloat(props.query.page || 1)} />
+  </div>
+);
 export default Home;
